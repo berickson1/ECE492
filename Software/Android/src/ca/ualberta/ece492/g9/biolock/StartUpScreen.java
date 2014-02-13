e6425c4eee94207d2302ac0542f6f897323e3e57
Mavis Chan <maviskay@ualberta.ca>
2014-02-13 00:29:24 -0700

    Added splash screen for android app

---
+++ Software/Android/src/ca/ualberta/ece492/g9/biolock/StartUpScreen.java
@@ -0,0 +1,30 @@
+/* Referenced: http://www.androidhive.info/2013/07/how-to-implement-android-splash-screen-2/ */
+package ca.ualberta.ece492.g9.biolock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+
+public class StartUpScreen extends FragmentActivity {
+	
+	private static final int SPLASHTIME = 2000;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_up_screen);  
+        
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(StartUpScreen.this, ItemListActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                //finish();
+            }
+        }, SPLASHTIME);
+    }
+}
