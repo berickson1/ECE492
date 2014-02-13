098d69e721eb4930e1bffe543e81113b752c76c8
Brent Erickson <brent2737@gmail.com>
2014-02-13 10:41:25 -0700

    Add non-volatile design

--- Quartus/BioLock.vhd
+++ Quartus/BioLock.vhd
@@ -63,7 +63,14 @@ entity BioLock is
 		ENET_RD_N	: 	out std_logic;
 		ENET_WR_N	:	out std_logic;
 		ENET_RST_N	:	out std_logic;
-		ENET_DATA	: 	inout std_logic_vector(15 downto 0)
+		ENET_DATA	: 	inout std_logic_vector(15 downto 0);
+		
+		FL_ADDR 		:	out std_logic_vector (21 downto 0);
+		FL_CE_N 		:	out std_logic_vector (0 downto 0);
+		FL_OE_N 		:	out std_logic_vector (0 downto 0);
+		FL_DQ 		:	inout std_logic_vector (7 downto 0);
+		FL_RST_N 	:	out std_logic_vector (0 downto 0);
+		FL_WE_N 		:	out std_logic_vector (0 downto 0)
 	
 	);
 end BioLock;
@@ -119,7 +126,13 @@ architecture structure of BioLock is
             dm9000a_if_0_s1_export_CS_N             : out   std_logic;                                        -- CS_N
             dm9000a_if_0_s1_export_RST_N            : out   std_logic;                                        -- RST_N
             dm9000a_if_0_s1_export_INT              : in    std_logic                     := 'X';             -- INT
-            dm9000a_if_0_s1_export_CLK              : out   std_logic    
+            dm9000a_if_0_s1_export_CLK              : out   std_logic;    
+				
+				tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_read_n_out       : out   std_logic_vector(0 downto 0);                     -- generic_tristate_controller_0_tcm_read_n_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_data_out         : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- generic_tristate_controller_0_tcm_data_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_chipselect_n_out : out   std_logic_vector(0 downto 0);                     -- generic_tristate_controller_0_tcm_chipselect_n_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_write_n_out      : out   std_logic_vector(0 downto 0);                     -- generic_tristate_controller_0_tcm_write_n_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_address_out      : out   std_logic_vector(21 downto 0)                     -- generic_tristate_controller_0_tcm_address_out
         );
     end component nios_system;
 
@@ -132,7 +145,8 @@ begin
 
 	DRAM_UDQM <= DQM(1);
 	DRAM_LDQM <= DQM(0);
-
+	FL_RST_N <= "1";
+	
     u0 : component nios_system
         port map (
             reset_reset_n                           => KEY(0),                           		     -- reset.reset_n
@@ -182,8 +196,13 @@ begin
             dm9000a_if_0_s1_export_CS_N             => ENET_CS_N,             --                                   .CS_N
             dm9000a_if_0_s1_export_RST_N            => ENET_RST_N,            --                                   .RST_N
             dm9000a_if_0_s1_export_INT              => ENET_INT,              --                                   .INT
-            dm9000a_if_0_s1_export_CLK              => ENET_CLK               --                                   .CLK
-
+            dm9000a_if_0_s1_export_CLK              => ENET_CLK,                                                       --                                   .CLK
+				
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_read_n_out       => FL_OE_N,       --      tristate_conduit_bridge_0_out.generic_tristate_controller_0_tcm_read_n_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_data_out         => FL_DQ,         --                                   .generic_tristate_controller_0_tcm_data_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_chipselect_n_out => FL_CE_N, --                                   .generic_tristate_controller_0_tcm_chipselect_n_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_write_n_out      => FL_WE_N,      --                                   .generic_tristate_controller_0_tcm_write_n_out
+            tristate_conduit_bridge_0_out_generic_tristate_controller_0_tcm_address_out      => FL_ADDR       --                                   .generic_tristate_controller_0_tcm_address_out
         );
 
 end structure;
