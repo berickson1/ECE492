cf194e419c4d766299cdfaad2bfd5dc2f3de7bb0
Mavis Chan <maviskay@ualberta.ca>
2014-02-13 13:15:19 -0700

    Changed splash screen to full screen

--- Software/Android/src/ca/ualberta/ece492/g9/biolock/StartUpScreen.java
+++ Software/Android/src/ca/ualberta/ece492/g9/biolock/StartUpScreen.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartUpScreen extends FragmentActivity {
 	
@@ -13,12 +15,16 @@ public class StartUpScreen extends FragmentActivity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Full screen without title bar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.start_up_screen);  
         
         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
+            	// Runs splash screen
                 Intent i = new Intent(StartUpScreen.this, MainActivity.class);
                 startActivity(i);
  
