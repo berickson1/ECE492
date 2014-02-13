2c53a316d29fa8312e6eeb762834162922375e49
Mavis Chan <maviskay@ualberta.ca>
2014-02-13 11:53:36 -0700

    Changed base project of app - from Fragment Activity to Activity

---
+++ Software/Android/BioLock/src/com/ece492/g9/biolock/DeviceList.java
@@ -0,0 +1,12 @@
+package com.ece492.g9.biolock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeviceList extends Activity{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
