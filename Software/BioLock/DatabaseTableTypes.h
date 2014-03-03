ae1c57faeca39726858d52fad3b71f84088449ce
Brent Erickson <brent2737@gmail.com>
2014-03-03 08:09:25 -0700

    Define structs for each database table

---
+++ Software/BioLock/DatabaseTableTypes.h
@@ -0,0 +1,47 @@
+/*
+ * Database_Table_Types.h
+ *
+ *  Created on: 2014-03-03
+ *      Author: be
+ */
+
+#ifndef DATABASE_TABLE_TYPES_H_
+#define DATABASE_TABLE_TYPES_H_
+using namespace std;
+
+typedef struct History{
+	int id, u_id;
+	bool success;
+	time_t time;
+} History;
+
+typedef struct User{
+	int id;
+	string name;
+	bool enabled;
+	time_t startDate, endDate;
+} User;
+
+typedef struct UserPrint{
+	int u_id, f_id;
+} UserPrint;
+
+typedef struct Role{
+	int id;
+	string name;
+	bool admin, enabled;
+	time_t startDate, endDate;
+} Role;
+
+typedef struct UserRole{
+	int u_id, r_id;
+	time_t startDate, endDate;
+} UserRole;
+
+typedef struct RoleSchedule{
+	int r_id, startTime, endTime, days;
+	time_t startDate, endDate;
+} RoleSchedule;
+
+
+#endif /* DATABASE_TABLE_TYPES_H_ */
