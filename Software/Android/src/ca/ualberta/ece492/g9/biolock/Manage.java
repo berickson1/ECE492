05236236927e73ea318734ada0e00ee59d99da45
Mavis Chan <maviskay@ualberta.ca>
2014-03-17 22:45:15 -0600

    Sends request to unlock door, need to check response

--- Software/Android/src/ca/ualberta/ece492/g9/biolock/Manage.java
+++ Software/Android/src/ca/ualberta/ece492/g9/biolock/Manage.java
@@ -1,7 +1,14 @@
+// TODO: Check if unlockDoor method is okay with JSONObject & handle response
+
 package ca.ualberta.ece492.g9.biolock;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,13 +16,18 @@ import android.view.WindowManager;
 
 // From AdminLogin - user authenticated & can modify settings
 public class Manage extends Activity {
-
+	public static final String PREFS_NAME = "CONNECTION";
+	String ip;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_manage);
+		// Gets the ip address
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		ip = settings.getString("ipAddress", "noConn");
 	}
 	
 	// Jumps to Users screen
@@ -37,8 +49,25 @@ public class Manage extends Activity {
 	}
 	
 	// Unlocks door lock
-	// TODO: Implement unlocking
 	public void unlockDoor(View v) {
-		System.out.println("Unlock door");
+		// JSONObject to request unlock door
+		JSONObject putRequest = new JSONObject();
+		try {
+			putRequest.put("request", "unlock");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// Post request to unlock door
+		JSONPost unlock = new JSONPost(new JSONCallbackFunction() {
+			@Override
+			public void execute(int response) {
+				// Not a valid response
+				if (response == -1){
+					System.out.println("No response");
+				}
+			}
+			public void execute(JSONArray json) {}
+		});
+		unlock.execute(ip.concat("/unlock"), ip.concat("/prints"), putRequest.toString());
 	}
 }
