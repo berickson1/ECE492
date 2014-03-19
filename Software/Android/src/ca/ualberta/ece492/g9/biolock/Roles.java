be339f120fc5c05c385f3a2c9fb4e11a678677b1
Mavis Chan <maviskay@ualberta.ca>
2014-03-18 21:51:42 -0600

    Updated role type & implemented displaying of roles

--- Software/Android/src/ca/ualberta/ece492/g9/biolock/Roles.java
+++ Software/Android/src/ca/ualberta/ece492/g9/biolock/Roles.java
@@ -1,77 +1,65 @@
 package ca.ualberta.ece492.g9.biolock;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+
+import ca.ualberta.ece492.g9.biolock.customs.JSONCallbackFunction;
+import ca.ualberta.ece492.g9.biolock.customs.JSONParser;
+import ca.ualberta.ece492.g9.biolock.customs.RoleAdapter;
+import ca.ualberta.ece492.g9.biolock.types.Role;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 //From Manage - user wishes to manage the roles
 public class Roles extends Activity {
+	public static final String PREFS_NAME = "CONNECTION";
+	private static String ip;
+	private static Context mContext;
+	
 	protected void onCreate(Bundle savedInstanceState) {
+		mContext = this;
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_roles);
-
+		
+		// Gets the ip address
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		ip = settings.getString("ipAddress", "noConn");
+		
+		// Obtains roles from web server & displays role names
 		JSONParser parser = new JSONParser(new JSONCallbackFunction() {
 			@Override
 			public void execute(JSONArray json) {
 				if (json != null) {
-					// String[] userID = new String[json.length()];
-					String[] name = new String[json.length()];
-					// String[] enabled = new String[json.length()];
-					// String[] startDate = new String[json.length()];
-					// String[] endDate = new String[json.length()];
-					ListView roleList;
-					ListAdapter displayRole;
-
-					roleList = (ListView) findViewById(R.id.listRoles);
-					ArrayList<HashMap<String, String>> roles = new ArrayList<HashMap<String, String>>();
-					for (int i = 0; i < json.length(); i++) {
-						try {
-							JSONObject role = (JSONObject) json.get(i);
-							// userID[i] = user.getString("id");
-							HashMap<String, String> rolesHash = new HashMap<String, String>();
-							rolesHash.put("Name", role.getString("name"));
-							roles.add(rolesHash);
-							/*
-							 * enabled[i] = user.getString("enabled");
-							 * startDate[i] = user.getString("startDate");
-							 * endDate[i] = user.getString("endDate");
-							 */
-							displayRole = new SimpleAdapter(getBaseContext(),
-									roles, R.layout.list_view_row,
-									new String[] { "Name" },
-									new int[] { R.id.listEntryName });
-							roleList.setAdapter(displayRole);
-						} catch (JSONException e) {
-							e.printStackTrace();
-						}
-					}
+					ListView roleList = (ListView) findViewById(R.id.listRoles);
+					ArrayList<Role> rolesArray = new ArrayList<Role>();
+					RoleAdapter adapter = new RoleAdapter(mContext, rolesArray);
+					Role role = new Role();
+					rolesArray = role.fromJson(json);
+					adapter.addAll(rolesArray);
+					roleList.setAdapter(adapter);
 				}
 			}
 			public void execute(Integer response) {}
 		});
-		//parser.execute("http://berickson.ca/ECE492/roles");
-		parser.execute("http://192.168.1.120/roles");
+		parser.execute(ip.concat("/roles"));
 	}
 
 	// User selected to add new role
 	public void addNewRole(View v) {
 		Intent newRole = new Intent(Roles.this, NewRole.class);
 		startActivity(newRole);
+		finish();
 	}
 }
--- Software/Android/src/ca/ualberta/ece492/g9/biolock/types/Role.java
+++ Software/Android/src/ca/ualberta/ece492/g9/biolock/types/Role.java
@@ -1,8 +1,68 @@
 package ca.ualberta.ece492.g9.biolock.types;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Role{
 	int id;
-	String name;
-	boolean admin, enabled;
+	public String name;
+	boolean admin;
+	public boolean enabled;
 	Long startDate, endDate;
+	
+	public Role(){
+		
+	}
+	
+	public Role(JSONObject jsonObjects){
+		try {
+			id = jsonObjects.getInt("id");
+			name = jsonObjects.getString("name");
+			admin = jsonObjects.getBoolean("admin");
+			enabled = jsonObjects.getBoolean("enabled");
+			startDate = jsonObjects.getLong("startDate");
+			endDate = jsonObjects.getLong("endDate");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+	
+    public ArrayList<Role> fromJson(JSONArray roleArray) {
+           ArrayList<Role> roles = new ArrayList<Role>();
+           for (int i = 0; i < roleArray.length(); i++) {
+               try {
+            	   roles.add(new Role(roleArray.getJSONObject(i)));
+               } catch (JSONException e) {
+                  e.printStackTrace();
+               }
+          }
+          return roles;
+    }
+    
+    public int getID(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public boolean getEnabled(){
+		return enabled;
+	}
+	
+	public boolean getAdmin(){
+		return admin;
+	}
+	
+	public Long getStartDate(){
+		return startDate;
+	}
+	
+	public Long getEndDate(){
+		return endDate;
+	}
 };
