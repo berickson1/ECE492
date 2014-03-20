9c101410f78d3d8c25f2e2a31053dcc1c8f6ea40
Mavis Chan <maviskay@ualberta.ca>
2014-03-20 00:18:08 -0600

    Added UserPrint adapter

---
+++ Software/Android/src/ca/ualberta/ece492/g9/biolock/customs/UserPrintAdapter.java
@@ -0,0 +1,42 @@
+// https://github.com/thecodepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+
+package ca.ualberta.ece492.g9.biolock.customs;
+
+import java.util.ArrayList;
+
+import ca.ualberta.ece492.g9.biolock.R;
+import ca.ualberta.ece492.g9.biolock.types.UserPrint;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class UserPrintAdapter extends ArrayAdapter<UserPrint> {
+	
+	public UserPrintAdapter(Context context, ArrayList<UserPrint> userPrints) {
+		super(context, R.layout.list_view_row_details, userPrints);
+	}
+	
+	@Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       // Get the data item for this position
+		UserPrint userPrint = getItem(position);    
+       
+       // Check if an existing view is being reused, otherwise inflate the view
+       if (convertView == null) {
+          convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view_row_details, null);
+       }
+       
+       // Lookup view for data population
+       TextView print = (TextView) convertView.findViewById(R.id.listDetailName);
+       
+       // Populate the data into the template view using the data object
+       print.setText(String.valueOf(userPrint.getID()));
+       
+       // Return the completed view to render on screen
+       return convertView;
+   }
+}
