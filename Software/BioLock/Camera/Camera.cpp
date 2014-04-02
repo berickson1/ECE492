b4095c116f457f67da91c45d436169cd195df81a
Brent Erickson <brent2737@gmail.com>
2014-04-02 16:48:59 -0600

    Camera works properlygit add -p

--- Software/BioLock/Camera/Camera.cpp
+++ Software/BioLock/Camera/Camera.cpp
@@ -14,18 +14,18 @@ const char Camera::BMPHEADER3[] = {0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x0
 char* Camera::getBMP(int * memsize){
 	bool cameraCaptureActive = true;
 	//TODO: Is this correct?
-	IOWR_ALTERA_AVALON_PIO_DATA(CAMERA_TRIGGER_BASE, cameraCaptureActive);
-	OSTimeDlyHMSM(0, 0, 1, 0);
-	{
+	//IOWR_ALTERA_AVALON_PIO_DATA(CAMERA_TRIGGER_BASE, cameraCaptureActive);
+	//OSTimeDlyHMSM(0, 0, 1, 0);
+	/*{
 
 		alt_up_av_config_dev* camConfig = alt_up_av_config_open_dev(CAMERA_NAME);
 		//Override config type
 		camConfig -> type = TRDB_D5M_CONFIG;
 		//Enable snapshot mode!
 		alt_up_av_config_write_D5M_cfg_register(camConfig, 0x0B, 1<<1);
-		OSTimeDlyHMSM(0, 0, 1, 0);
+		OSTimeDlyHMSM(0, 0, 0, 200);
 		alt_up_av_config_write_D5M_cfg_register(camConfig, 0x0B, 1<<1 | 1<<0);
-	}
+	}*/
 	*memsize = CAM_WIDTH*CAM_HEIGHT*4 + BMPHEADERLEN;
 	OSTimeDlyHMSM(0, 0, 1, 0);
 	char * dataBuff = (char*)malloc((*memsize));
@@ -49,14 +49,14 @@ char* Camera::getBMP(int * memsize){
 	memcpy((void*)(dataBuff + offset), (void*)&BMPHEADER3, BMPHEADER3LEN);
 	offset += BMPHEADER3LEN;
 	imageToBuffer(dataBuff + offset);
-	IOWR_ALTERA_AVALON_PIO_DATA(CAMERA_TRIGGER_BASE, !cameraCaptureActive);
-	{
+	//IOWR_ALTERA_AVALON_PIO_DATA(CAMERA_TRIGGER_BASE, !cameraCaptureActive);
+	/*{
 		alt_up_av_config_dev* camConfig = alt_up_av_config_open_dev(CAMERA_NAME);
 		//Override config type
 		camConfig -> type = TRDB_D5M_CONFIG;
 		//Enable snapshot mode!
 		alt_up_av_config_write_D5M_cfg_register(camConfig, 0x0B, 1<<0);
-	}
+	}*/
 	return dataBuff;
 }
 
@@ -69,28 +69,31 @@ char* Camera::getRawImage(){
 
 void Camera::imageToBuffer(char* buffer){
 	int imagesize = CAM_WIDTH*CAM_HEIGHT*4;
-	memcpy((void*)(buffer), (void*)SRAM_0_BASE,  imagesize);
+	for (int i = 0; i < CAM_HEIGHT; i++){
+		memcpy((void*)(buffer + ((CAM_HEIGHT - i - 1) * CAM_WIDTH * 4)), (void*)((char*)SRAM_0_BASE + (i * CAM_WIDTH * 4)), CAM_WIDTH*4);
+	}
+
 }
 void Camera::init(){
 	alt_up_av_config_dev* camConfig = alt_up_av_config_open_dev(CAMERA_NAME);
 	//Override config type
 	camConfig -> type = TRDB_D5M_CONFIG;
-	OSTimeDlyHMSM(0, 0, 1, 0);
+	//OSTimeDlyHMSM(0, 0, 1, 0);
 	//Mirror Rows and Columns
-	alt_up_av_config_write_D5M_cfg_register(camConfig, 0x20, 0xC000);
+	//alt_up_av_config_write_D5M_cfg_register(camConfig, 0x20, 0xC000);
 	//PLL On
-	alt_up_av_config_write_D5M_cfg_register(camConfig, 0x10, 0x51);
+	/*alt_up_av_config_write_D5M_cfg_register(camConfig, 0x10, 0x51);
 	//Set PLL Factor M and N
 	alt_up_av_config_write_D5M_cfg_register(camConfig, 0x11, 0x2804);
 	OSTimeDlyHMSM(0, 0, 1, 0);
 	//Set PLL Dividor to 39
-	alt_up_av_config_write_D5M_cfg_register(camConfig, 0x12, 0x28);
+	alt_up_av_config_write_D5M_cfg_register(camConfig, 0x12, 0x38);
 	//Use PLL
 	alt_up_av_config_write_D5M_cfg_register(camConfig, 0x10, 0x53);
 	OSTimeDlyHMSM(0, 0, 1, 0);
 	alt_up_av_config_write_D5M_cfg_register(camConfig, SNAPSHOT_MODE_REG, SNAPSHOT_MODE_VAL);
-	OSTimeDlyHMSM(0, 0, 1, 0);
-	alt_up_av_config_write_D5M_cfg_register(camConfig, HORIZONTAL_REG, HORIZONTAL_VAL);
+	OSTimeDlyHMSM(0, 0, 1, 0);*/
+	//alt_up_av_config_write_D5M_cfg_register(camConfig, HORIZONTAL_REG, HORIZONTAL_VAL);
 }
 
 
